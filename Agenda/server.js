ec3eb5afb4745e316bab7e093e753b659cd76c34
gTarcizo <90656673+gTarcizo@users.noreply.github.com>
2021-12-22 20:51:38 -0300

    Colocando imagem com EJS

--- Agenda/server.js
+++ Agenda/server.js
@@ -7,20 +7,21 @@ mongoose.connect(process.env.CONNECTIONSTRING, { useNewUrlParser: true, useUnifi
     app.emit('pronto');
   })
   .catch(e => console.log(e));
-const session = require('express-session');
-const MongoStore = require('connect-mongo');
-const flash = require('connect-flash');
-const routes = require('./routes');
-const path = require('path');
-const helmet = require('helmet');
-const csrf = require('csurf');
-const { middlewareGlobal, checkCsrfError, csrfMiddleware } = require('./src/middlewares/middleware');
-
-app.use(helmet());
-
-app.use(express.urlencoded({ extended: true }));
+  const session = require('express-session');
+  const MongoStore = require('connect-mongo');
+  const flash = require('connect-flash');
+  const routes = require('./routes');
+  const path = require('path');
+  const helmet = require('helmet');
+  const csrf = require('csurf');
+  const { middlewareGlobal, checkCsrfError, csrfMiddleware } = require('./src/middlewares/middleware');
+  
+  app.use(helmet());
+  
+  app.use( express.static( "public" ) );
+  app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.resolve(__dirname, 'public')));
+//app.use(express.static(path.resolve(__dirname, 'public')));
 
 const sessionOptions = session({
   secret: 'akasdfj0út23453456+54qt23qv  qwf qwer qwer qewr asdasdasda a6()',
